4750babca51c60d186de0cc383baaca997e50de8
Joseph Enoch <enochjoseph01@gmail.com>
2023-01-10 13:29:54 +0100

    add check for NFC support

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import nfcManager from 'react-native-nfc-manager';
 
 export default function App() {
+  const [isNfcSupported, setIsNfcSupported] = useState<boolean | null>(null)
+
+  useEffect(()=>{
+    const checkSupport = async () => {
+      const supports = await nfcManager.isSupported()
+      setIsNfcSupported(supports)
+    }
+    checkSupport()
+  },[])
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{isNfcSupported ? "Your Device Supports NFC" : "Your Device doesn't support NFC"}</Text>
     </View>
   );
 }
