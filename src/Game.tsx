bf7e9cf911f3a0e1c97da842e4345d7f7c99af33
Joseph Enoch <enochjoseph01@gmail.com>
2023-01-12 11:30:24 +0100

    complete the nfc game

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,23 +1,36 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import nfcManager, { NfcEvents } from 'react-native-nfc-manager'
 
 const Game = () => {
+  const [duration, setDuration] = useState<number | null>(null)
+  const [startTime, setStartTime] = useState<number | null>(null)
     useEffect(()=>{
+      let count = 5
         nfcManager.setEventListener( NfcEvents.DiscoverTag, (tag:any)=>{
+            count--
+            nfcManager.setAlertMessage(`Remaining ${count} tags`)
             console.warn(tag)
         })
+        if(count <=0 && startTime){
+          nfcManager.unregisterTagEvent().catch(()=>0)
+          setStartTime(0)
+          setDuration(new Date().getTime() - startTime)
+        }
         return ()=> nfcManager.setEventListener(NfcEvents.DiscoverTag, null)
-    },[])
+    },[startTime])
     
     const scanTag = async () =>{
+      setStartTime(new Date().getTime())
         await nfcManager.registerTagEvent()
+        setDuration(0)
     }
 
   return (
     <View>
 
       <Text>NFC Game</Text>
+      {duration && <Text>{duration} ms</Text>}
       <TouchableOpacity onPress={scanTag}>
         Start Game
       </TouchableOpacity>
